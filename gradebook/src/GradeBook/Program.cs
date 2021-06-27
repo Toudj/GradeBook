84af0c9d53e4ed2358036318a0d1245cd6ec10e3
Toudj <mohamedtidjanioumarou@gmail.com>
2021-06-27 12:25:02 +0200

    first commit

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Toudj";
+            Console.WriteLine("Hello " + name + " !");
+           
+        }
+    }
+  
+}
