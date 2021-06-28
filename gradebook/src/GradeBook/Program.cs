ae7a837767e4a7adb5577e9d54f315461385d40b
Toudj <mohamedtidjanioumarou@gmail.com>
2021-06-28 17:32:39 +0200

    Refactoring

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -7,16 +7,16 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-           var book = new Book("Science"); 
-           book.AddGrade(89.1);
-           book.AddGrade(100);
-           book.AddGrade(50);
-           book.AddGrade(2);
-           book.AddGrade(18);
+            var book = new Book("Science"); 
+            book.AddGrade(89.1);
+            book.AddGrade(100);
+            book.AddGrade(50);
+            book.AddGrade(2);
+            book.AddGrade(18);
 
             var stats = book.GetStatistics();
 
-           Console.WriteLine($"Total of Grades : {stats.Total} ");
+            Console.WriteLine($"Total of Grades : {stats.Total} ");
             Console.WriteLine($"Number of students : {stats.nStudents} ");
             Console.WriteLine($"Average grade : {stats.Average:N1} ");
             Console.WriteLine($"The Lowest grade : {stats.Low:N1}");
